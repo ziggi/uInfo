b6d3a116f0ce323601ef6bd3afea789c9e207524
Sergey Marochkin <xziggix@gmail.com>
2013-07-29 22:14:48 +0400

    uInfo v1.6, rewrite index.php, remove all javascript code

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include "uinfo.class.php";
 
 define("SITE_URL","http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
-define("ENGINE_VERSION","1.4");
+define("ENGINE_VERSION","1.6");
 
 $user = new uInfo($_SERVER["HTTP_USER_AGENT"]);
 
@@ -12,73 +12,59 @@ $user = new uInfo($_SERVER["HTTP_USER_AGENT"]);
 <html>
 
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta charset="utf-8">
 	<meta name="description" content="Узнать IP адрес, браузер и ОС">
 	<meta name="keywords" content="ip адрес, узнать ip адрес, мой ip, мой браузер, моя ос, узнать браузер, узнать ОС">
 	<title>uInfo - Информация о вас</title>
 	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
-	<link rel="stylesheet" type="text/css" href="style.css"> 
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" type="text/css" href="zishell/style.css">
-	<script type="text/javascript" src="jscripts/jquery-1.9.0.min.js"></script>
-	<script type="text/javascript" src="zishell/scripts.js"></script>
 </head>
 
 <body>
-	<div id="zishell">
-		<div class="menu">
-			<div style="float:left"></div>
-			<div style="float:right">
-				<span>
-					<a href="https://github.com/ziggi/uInfo" target="_blank">v<?=ENGINE_VERSION?></a>
-					&nbsp;
-					<a href="http://ziggi.org/" target="_blank">ziggi</a>
-				</span>
+
+<div id="zishell">
+	<div class="menu">
+		<div style="float:left"></div>
+		<div style="float:right">
+			<span>
+				<a href="https://github.com/ziggi/uInfo" target="_blank">v<?=ENGINE_VERSION?></a>
+				&nbsp;
+				<a href="http://ziggi.org/" target="_blank">ziggi</a>
+			</span>
+		</div>
+	</div>
+</div>
+
+<div id="middle">
+	<div id="text">
+		<div id="ip"><?=$user->host("ip")?></div>
+		<div id="list">
+			<table>
+				<tr><td>Имя компьютера:</td><td><?=$user->host("name")?></td></tr>
+				<tr><td>Операционная система:</td><td><?=$user->os("name")." ".$user->os("version")?></td></tr>
+				<tr><td>Браузер:</td><td><?=$user->browser("name")." ".$user->browser("version")?></td></tr>
+			</table>
+		</div>
+	</div>
+	
+	<div id="img">
+		<div id="image">
+			<img src="<?=SITE_URL?>image.jpg">
+		</div>
+		<div id="img_links">
+			<div class="link">
+				<div class="link_text">HTML код:</div>
+				<textarea onClick="select()"><img src="<?=SITE_URL?>image.jpg"></textarea>
+			</div>
+			<div class="link">
+				<div class="link_text">BB код:</div>
+				<textarea onClick="select()">[BB]<?=SITE_URL?>image.jpg[/BB]</textarea>
 			</div>
 		</div>
 	</div>
-	<div class="div_form">
-		<table>
-			<tr>
-				<td class="info_ip" colspan="2"><?=$user->host("ip")?></td>
-			</tr>
-			<tr>
-				<td class="info_type">Имя компьютера:</td>
-				<td class="info_result"><?=$user->host("name")?></td>
-			</tr>
-			<tr>
-				<td class="info_type">Операционная система:</td>
-				<td class="info_result"><?=$user->os("name")." ".$user->os("version")?></td>
-			</tr>
-			<tr>
-				<td class="info_type">Браузер:</td>
-				<td class="info_result"><?=$user->browser("name")." ".$user->browser("version")?></td>
-			</tr>
-		</table>
-		
-		<hr class="hr_0">
-		
-		<div class="info_image">
-		<table>
-			<tr>
-				<td>
-					<p class="p_center"><img src="<?=SITE_URL?>image.jpg" alt="image_banner"></p>
-					<hr class="hr_0">
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<p class="p_center">HTML Код:</p>
-					<textarea onClick="select()"><img src="<?=SITE_URL?>image.jpg"></textarea>
-				</td>
-			<tr>
-				<td>
-					<p class="p_center">BB Код:</p>
-					<textarea onClick="select()">[img]<?=SITE_URL?>image.jpg[/img]</textarea>
-				</td>
-			</tr>
-		</table>
-		</div>
-	</div>
+</div>
+
 </body>
 
 </html>
