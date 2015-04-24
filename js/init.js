290b5e0c6397b229ab29c58b7e4ceed68447741f
ziggi <xziggix@gmail.com>
2015-04-24 19:24:51 +0300

    added js and locales files

---
+++ js/init.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function() {
+	var event_init = new Event('init');
+
+	// i18n init
+	var option = {
+		resGetPath: 'locales/__ns_____lng__.json',
+		fallbackLng: 'en'
+	};
+
+	i18n.init(option, function() {
+		[].forEach.call(document.querySelectorAll('[data-i18n]'), function(element) {
+			if (element.dataset.i18n != i18n.t(element.dataset.i18n)) {
+				element.innerHTML = i18n.t(element.dataset.i18n);
+			}
+		});
+
+		document.title = i18n.t('app.title', {appName: i18n.t('app.name')});
+		
+		elem = document.querySelector('[data-i18n="app.fullname"]');
+		elem.innerHTML = i18n.t('app.name') + ' ' + elem.innerHTML;
+
+		document.dispatchEvent(event_init);
+	});
+
+});
